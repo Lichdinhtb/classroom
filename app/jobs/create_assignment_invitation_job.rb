aef2d4ad099c38c13d2bfe91b301c6e4a4382b8a
Mark Tareshawty <mark@marktareshawty.com>
2015-06-26 16:47:08 -0500

    WIP

--- app/jobs/create_assignment_invitation_job.rb
+++ app/jobs/create_assignment_invitation_job.rb
@@ -1,6 +1,11 @@
 class CreateAssignmentInvitationJob < ActiveJob::Base
   queue_as :default
 
+  # Public: Create an invitation for a given assignment
+  #
+  # assignment - The Assignment that needs an invitation
+  #
+  # Returns if the save was successful
   def perform(assignment)
     invitation = assignment.build_assignment_invitation
     invitation.save!
